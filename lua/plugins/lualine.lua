9650c90480b72e0781a54f85111798cc85ad472c
Nicholas Brock <137735440+nicholasrlb@users.noreply.github.com>
2023-10-27 18:10:30 -0400

    Add lualine plugin Added the lualine plugin. It uses default settings and default formatting for the options, but it works. Will continue to personalize in the future.

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,35 @@
+return {
+	'nvim-lualine/lualine.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
+	lazy = false,
+
+	opts = function()
+		return {
+			options = {
+				icons_enabled = true,
+				theme = 'auto',
+				component_separators = { left = '', right = ''},
+				section_separators = { left = '', right = ''},
+				always_divide_middle = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				}
+			},
+			sections = {
+				lualine_a = {'mode'},
+				lualine_b = {'branch', 'diff', 'diagnostics'},
+				lualine_c = {'filename'},
+				lualine_x = {'encoding', 'fileformat', 'filetype'},
+				lualine_y = {'progress'},
+				lualine_z = {'location'}
+			},
+			inactive_sections = {
+				lualine_c = {'filename'},
+				lualine_x = {'location'},
+			},
+		}
+	end
+}
